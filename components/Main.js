4d833e80ab4d4afffb8e7df9d1bc6ecc46f5a189
rom <r.paris@it-students.fr>
2019-05-07 09:18:19 +0200

    Mon supere projet

---
+++ components/Main.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity }
+  from 'react-native';
+import Note from './Note';
+
+
+
+
+export default class Main extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: '',
+    }
+  }
+  render() {
+    let notes = this.state.noteArray.map((val, key)) => {
+      return <Note key={key} keyval={key} val={val}
+        deleteMethod={() => this.deleteNote(key)}
+      />
+    )
+    };
+  }
+
+  return(
+      <View style={styles.container }>
+
+        <View style={styles.header}>
+          <Text style={styles.headerText}>- PRISE DE NOTES -</Text>
+        </View>
+
+      <ScrollView style={styles.scrollContainer}>
+      </ScrollView>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.TextInput}
+            onChangeText={(noteText) => this.setState({noteText})}
+            value={this.state.noteText}
+            placeholder='>note'
+            placeholderTextColor='white'
+            inderlineColorAndroid='transparent'>
+          </TextInput>
+        </View>
+        <TouchableOpacity onPress={ this.addNote.bind(this) } styles={styles.addButton}>
+          <Text style={styles.ButtonText}> + </Text>
+        </TouchableOpacity>
+      </View>
+);
+ 
+
+  addNote() {
+//alert ('test pour voir la note');
+    if (this.state.noteText)  { 
+      var d = new Dtae ();
+      this.state.noteArray.push
+    });
+     
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "#FF5733",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 10,
+    borderBottomColor: "#ddd"
+  },
+  headerText: {
+    color: "white",
+    fontSize: 36,
+    padding: 26
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10
+  },
+  textInput: {
+    alignSelf: "stretch",
+    color: "#fff",
+    padding: 20,
+    backgroundColor: "#252525",
+    borderTopWidth: 2,
+    borderTopColor: "#ededed"
+  },
+  addButton: {
+    position: "absolute",
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: "#33FF65",
+    width: 90,
+    height: 90,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 8
+  },
+  addButtonText: {
+    color: "#33FF65",
+    fontSize: 24
+  },
+});
